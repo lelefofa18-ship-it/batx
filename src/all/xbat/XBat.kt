22f8e212c876335138ac6e9f5b7c35ded2ea543f
lelefofa18-ship-it <lelefofa18@gmail.com>
2026-01-21 15:23:26 +0100

    add xbat source

---
+++ src/all/xbat/XBat.kt
@@ -0,0 +1,47 @@
+package eu.kanade.tachiyomi.extension.all.xbat
+
+import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import eu.kanade.tachiyomi.source.model.*
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+
+class XBat : ParsedHttpSource() {
+
+    override val name = "XBAT.IO"
+    override val baseUrl = "https://xbat.io"
+    override val lang = "all"
+    override val supportsLatest = true
+
+    override fun popularMangaSelector() = "a[href^=/title/]"
+
+    override fun popularMangaFromElement(element: Element): SManga =
+        SManga.create().apply {
+            title = element.text()
+            url = element.attr("href")
+        }
+
+    override fun popularMangaNextPageSelector() = null
+
+    override fun mangaDetailsParse(document: Document): SManga =
+        SManga.create().apply {
+            description =
+                document.select("meta[name=description]").attr("content")
+            thumbnail_url =
+                document.select("img").firstOrNull()?.attr("abs:src")
+        }
+
+    override fun chapterListSelector() = "a[href*=-ch_]"
+
+    override fun chapterFromElement(element: Element): SChapter =
+        SChapter.create().apply {
+            name = element.text()
+            url = element.attr("href")
+        }
+
+    override fun pageListParse(document: Document): List<Page> =
+        document.select("img").mapIndexed { i, img ->
+            Page(i, "", img.attr("abs:src"))
+        }
+
+    override fun imageUrlParse(document: Document) = ""
+}
