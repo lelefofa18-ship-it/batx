a0cc9a93a44fef7dc4589ff975c4438c44f0e28b
lelefofa18-ship-it <lelefofa18@gmail.com>
2026-01-22 22:50:34 +0100

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,19 +1,1 @@
-const puppeteer = require('puppeteer'); // vai abrir o site e pegar as imagens
 
-(async () => {
-  // URL do capítulo que você quer testar
-  const chapterUrl = 'COLE_AQUI_A_URL_DO_CAPITULO';
-
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-  await page.goto(chapterUrl);
-
-  // Seleciona todas as imagens usando o seletor CSS
-  const imageUrls = await page.$$eval('img.w-full.h-full', imgs =>
-    imgs.map(img => img.src)
-  );
-
-  console.log(imageUrls); // imprime os links das imagens
-
-  await browser.close();
-})();
